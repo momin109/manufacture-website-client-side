891c7218e0925d970deae7b56e297eae9f8b3d70
momin109 <momin550550@gmail.com>
2022-05-26 09:57:47 +0600

    my profile added

--- src/Page/Home/Portfolia/Portfolia.js
+++ src/Page/Home/Portfolia/Portfolia.js
@@ -16,12 +16,20 @@ const Portfolia = () => {
     }
     return (
         <div>
-            <form className='grid grid-cols-1 gap-3 justify-items-center mt-2'>
 
-                <h1 className='text-2xl font-bold '>{user.displayName || ''}</h1>
-                <h2>{user.email || ''}</h2>
-                <h3>{user.address}</h3>
-            </form>
+            <div class=" max-w-7xl bg-base-100 shadow-xl lg:mx-32">
+                <div class="card-body">
+                    <h2 class=" text-2xl font-bold text-amber-400'">Welcome to Your Profolia website</h2>
+                    <h1 className='text-3xl font-bold text-amber-400'>Name: {user.displayName || ''}</h1>
+                    <h2 className='text-amber-700 text-4xl'>Email Address: {user.email || ''}</h2>
+                    <h3 className='text-2xl'>Address: house/holding: 56/2, South Charmohanpur Jame  Mosque , Tikrampur ,
+                        Chapai Nawabganj sadar ,
+                        chapai nawabganj</h3>
+                    <h1 className='text-2xl mb-20'>Phone No. 017977XXXXXX</h1>
+                    <h2>My Live website Link</h2>
+                    <a href="https://warehouse-management-cli-dcfa7.web.app">First website live link</a>
+                </div>
+            </div>
         </div>
     );
 };
